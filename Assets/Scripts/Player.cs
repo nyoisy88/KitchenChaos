dac28179606397d73a624e00cabc005a61c3a801
Vinhnt <vinhvn102@gmail.com>
2025-05-04 23:42:46 +0700

    little fix

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    #region unity methods
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    #endregion
+    
+}
